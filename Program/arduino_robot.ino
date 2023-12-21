08c6fdf63a5aeccd52df9f6a37abbfd0345e8ff8
med0amine <151380756+med0amine@users.noreply.github.com>
2023-12-21 04:58:25 -0800

    arduino_robot.ino

--- Program/arduino_robot.ino
+++ Program/arduino_robot.ino
@@ -1,3 +1,18 @@
+/*
+   Copyright [2023] [Mohamed Amine Ben Ammar]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 const int LIGHT_SENSOR_PIN = A0;
 const int LED_PIN          = 13;
 const int ANALOG_THRESHOLD = 20;
